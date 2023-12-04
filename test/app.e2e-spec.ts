140eac2cdc33c6e0cca8d4adea843d94b931a7d8
alank976 <alankan976@gmail.com>
2023-12-04 23:52:19 +1100

    setup infra for e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,23 +2,34 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+// import { asyncScheduler } from 'rxjs';
 
-describe('AppController (e2e)', () => {
+describe('Users', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  beforeAll(
+    async () => {
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+        imports: [AppModule],
+      }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+      app = moduleFixture.createNestApplication();
+      await app.init();
+    }, 10 * 60_000
+  );
+
+  afterAll(async () => {
+    await app.close();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('Can register a user' , ()=> {
+    // TODO
+  })
+
+  it('Can get new registered user)', () => {
+      return request(app.getHttpServer())
+        .get('/users')
+        .expect(200)
+        .expect([]); // TODO
   });
 });
