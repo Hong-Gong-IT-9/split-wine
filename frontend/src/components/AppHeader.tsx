0637962b60f18ef03018cfecc9a0be987d4b1bef
KathyL <wyl987@uowmail.edu.au>
2024-02-04 21:44:13 +1100

    AppHeader - added buttons after login

--- frontend/src/components/AppHeader.tsx
+++ frontend/src/components/AppHeader.tsx
@@ -2,11 +2,39 @@ import {Button} from "@mui/material";
 import logo from '../images/logo.png'
 import style from './AppHeader.module.css'
 import { useNavigate } from 'react-router-dom';
+import React from "react"
+
 const AppHeader = () => {
 
     const navigate = useNavigate();
 
-    return <div className={style['container']}>
+    const [login, isLogin] = React.useState(true)
+
+    return login ? (
+        <div className={style['container']}>
+        <img src={logo} alt="wine-logo"/>
+        <div className={style['button-group']}>
+            <Button onClick={()=> {
+                navigate('/dashboard')
+            }} style={{textTransform: 'none', color: '#E85757'}}>Dashboard</Button>
+            <Button onClick={()=> {
+                navigate('/event')
+            }} style={{textTransform: 'none', color: '#E85757'}}>My Events</Button>
+            <Button onClick={()=> {
+                navigate('/wine')
+            }} style={{textTransform: 'none', color: '#E85757'}}>My Wines</Button>
+            <Button onClick={()=> {
+                navigate('/friend')
+            }} style={{textTransform: 'none', color: '#E85757'}}>Frient List</Button>
+            <Button
+                onClick={()=> {
+                    navigate('/')
+                }}
+                style={{textTransform: 'none', color: '#E85757'}}>Sign out</Button>
+        </div>
+    </div>
+    ) : (
+        <div className={style['container']}>
         <img src={logo} alt="wine-logo"/>
         <div className={style['button-group']}>
             <Button onClick={()=> {
@@ -19,6 +47,7 @@ const AppHeader = () => {
                 style={{textTransform: 'none', backgroundColor: '#E85757', color: '#FFFFFF'}}>Sign Up</Button>
         </div>
     </div>
+    )    
 }
 
 export default AppHeader;
