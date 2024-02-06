3edce58c04aa387ee7fceadcec8ed6f995054445
alank <alankan976@gmail.com>
2024-02-06 17:02:12 +1100

    openapi codegen

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,48 @@
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  SelectChangeEvent,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import { User as UserDto } from "../generated/openapi/api";
+import Api from "../Api";
+
 const Login = () => {
-    return <div>login page</div>
-}
+  const [userId, setUserId] = useState("");
+  const [userOptions, setUserOptions] = useState(new Array<UserDto>());
+
+  useEffect(() => {
+    Api.User.getUsers().then((response) => {
+      if (response.data) {
+        setUserOptions(response.data);
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <p>login page</p>
+      <p>For demo purpose only below:</p>
+      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={userId}
+          label="User"
+          onChange={(event: SelectChangeEvent) => {
+            setUserId(event.target.value);
+          }}
+        >
+          {userOptions.map((u: UserDto) => (
+            <MenuItem value={u.id?.toString()}>{u.name}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+};
 
 export default Login;
