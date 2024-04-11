4d359ed92879e71ab9c8fe36f25c6110ffd7f702
Wai Chun TSANG <waichun_2009@hotmail.com>
2024-04-11 23:56:20 +1000

    Modified Prisma Schema and Added user registration module (1st attempt)

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './users.module';
 
@@ -9,5 +9,14 @@ export class UsersController {
   @Get()
   async getUsers(): Promise<User[]> {
     return this.usersService.getUsers();
+  }
+
+  @Post()
+  async regUser(
+    @Body('name') name: string,
+    @Body('email') email: string,
+    @Body('password') password: string
+  ): Promise<User> {
+    return this.usersService.regUser(name, email, password);
   }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -9,4 +9,19 @@ export class UsersService {
   async getUsers(): Promise<User[]> {
     return await this.prisma.user.findMany();
   }
+
+  async regUser(name: string, email: string, plainPassword: string) {
+    // TODO: might need to hash the password later on
+    
+    // create new user in database
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        email,
+        password: plainPassword,
+      },
+    });
+
+    return user;
+  }
 }
